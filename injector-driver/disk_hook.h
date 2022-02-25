855464cb3632b45aea5210a42174fad26c1252ae
MellowNight <89277220+MellowNight@users.noreply.github.com>
2022-02-24 22:35:35 -0500

    asds

--- injector-driver/disk_hook.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "util.h"
-
-typedef __int64(__fastcall* pfnWppTraceMessage)(__int64, __int64, __int64, __int64, __int64);
-
-extern pfnWppTraceMessage o_WppTraceMessage;
-extern uintptr_t ioctl_ret_addr;
-
-void DiskHookHandler();
