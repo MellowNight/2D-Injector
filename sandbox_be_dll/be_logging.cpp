902ebc3931935847300c3f5088dcb473f89d8fa6
MellowNight <89277220+MellowNight@users.noreply.github.com>
2022-11-26 22:59:08 -0500

    start testing memory access callback

--- sandbox_be_dll/be_logging.cpp
+++ sandbox_be_dll/be_logging.cpp
@@ -6,16 +6,16 @@
 
 void ExecuteHook(GeneralRegisters* registers, void* return_address, void* o_guest_rip)
 {
-	Logger::Get()->Print(COLOR_ID::green, "[EXECUTE] ");  
+	Logger::Get()->Print(COLOR_ID::green, "[EXECUTE]	");  
 	Logger::Get()->Print(COLOR_ID::none, "return_address 0x%p \n", return_address);
 
-	Logger::Get()->Print(COLOR_ID::green, "[EXECUTE]  ");
+	Logger::Get()->Print(COLOR_ID::green, "[EXECUTE]	");
 	Logger::Get()->Print(COLOR_ID::none, "rip = 0x%p \n", o_guest_rip);
 }
 
 void ReadWriteHook(GeneralRegisters* registers, void* o_guest_rip)
 {
-	Logger::Get()->Print(COLOR_ID::magenta, "[READ/WRITE]  ");
+	Logger::Get()->Print(COLOR_ID::magenta, "[READ/WRITE]	");
 	Logger::Get()->Print(COLOR_ID::none, "rip = 0x%p \n", o_guest_rip);
 }
 
@@ -30,6 +30,10 @@ void SandboxRegion(uintptr_t base, uintptr_t size)
 
 		ForteVisor::SandboxPage((uintptr_t)offset, NULL);
 	}
+
+	auto kernel32 = GetModuleHandleA("kernel32.dll");
+
+	ForteVisor::DenySandboxMemoryAccess(kernel32 + 0x1000);
 }
 
 void StartBELogger()
